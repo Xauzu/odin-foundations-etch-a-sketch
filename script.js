65d24ae1d286678d0aebb6205f46525a56cdc180
Xau <frozzy.xfs@gmail.com>
2023-06-24 08:34:38 -0400

    Adjust cell creation

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test");
+const gridWidth = 16;
+const gridHeight = 16;
+
+function populate() {
+    const grid = document.querySelector('.grid');
+    for(let i = 0; i < gridHeight; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < gridWidth; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+
+populate();
