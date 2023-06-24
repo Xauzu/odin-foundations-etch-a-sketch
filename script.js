8e3cac4b68830800ccc7fd8ceaab06c61a2b8251
Xau <frozzy.xfs@gmail.com>
2023-06-24 09:14:45 -0400

    Empty grid on populate

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Populate grid
 function populate(width, height) {
     const grid = document.querySelector('.grid');
+    
+    // Empty Grid
+    while (grid.hasChildNodes)
+        grid.removeChild(grid.lastChild);
+
     for(let i = 0; i < height; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -12,6 +18,13 @@ function populate(width, height) {
         grid.appendChild(row);
     }
     document.querySelector(".dimensionDisplay").textContent = width + " x " + height;
+}
+
+// Change grid to new dimension
+function changeDimension() {
+    const newWidth = prompt("Enter a new Width", "16");
+    const newHeight = prompt("Enter a new Height", "16");
+    populate(newWidth, newHeight);
 }
 
 // Initial Width, Height
